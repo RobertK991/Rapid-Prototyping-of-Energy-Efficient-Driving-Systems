0f6d129a6d0a13d4b7e9886cc0e18209242061dd
RobertK991 <robertkowal96@gmail.com>
2020-12-17 18:34:58 +0100

    Zrobiono do 11 punktu

--- start_model_silnik.m
+++ start_model_silnik.m
@@ -6,6 +6,7 @@ stopTime = 80;
 
 model.licznik = 0.314656258763423;
 model.mianownik = [1,1.00277037976700,0];
+
 %% Char. statyczna
 clear all
 h = 0.01;
@@ -54,7 +55,7 @@ iden = ones(w,1)*u_0;
 systemIdentification
 
 %% K
-K = tf(tf2.Numerator, tf2.Denominator)
+% K = tf(tf2.Numerator, tf2.Denominator)
 
 % Fit to estimation data: 99%, FPE: 0.000320976
 
@@ -76,12 +77,13 @@ K = tf(tf2.Numerator, tf2.Denominator)
 figure;
 plot(y,'r');
 hold on;
-step(tf1,20);
+step(K,20);
 
 title('Compare')
 legend('Plant','Model');
 grid on;
-
+ylabel('y [rad]');
+xlabel('u [V]');
 %% Punkt 5 i 6
 % Regulator PID 
 % Regulator P 
@@ -90,11 +92,11 @@ load('K.mat');
 
 h = 0.01;
 uMax = 2;
-iteracje = 3;
+iteracje = 4;
 stopTime = 50;
 
 for i = 0:iteracje
-    u = (0.3 + 0.2*i)*uMax;
+    u = (0.3 + 0.1*i)*uMax;
     [yStep, tStep] = step(K*u,0:h:stopTime);
     yMean = mean(yStep(end-100:end));
     eps = 0.5;
@@ -131,11 +133,14 @@ regPID.Td = mean(regPID.Td);
 % Dane modelu
 model.licznik = 0.314656258763423;
 model.mianownik = [1,1.00277037976700,0];
+disp(['Nastawy regulatora P: kr = ', num2str(regP.kr)]);
+disp(['Nastawy regulatora PID: kr = ', num2str(regPID.kr),' Ti = ',num2str(regPID.Ti),' Td = ',num2str(regPID.Td)]);
 
+save('nastawy', 'regP', 'regPID')
 %% Symulacja regulator P i PID
 
 % Wartość zadana
-u = 2.5;
+u = 1;
 stopTime = 80;
 
 %Nastawy obliczone
@@ -143,46 +148,72 @@ load('nastawy.mat');
 
 sub.F = 1;
 sub.T = 1;
-strojenieReczne('model7.slx', regP, regPID, sub);
+strojenieReczne('model7.slx', regP, regPID, sub,[],[]);
 
 
 %% Punkt 8
+close all;
 stopTime = 80;
-uInit = [-2, 2, pi];
+uInit = [-1, 1, pi];
+set = [];   % puste - żeby wyśweitlić setValue
+sub.F = 1;
+sub.T = 1;
+u = uInit(1);
+kolory = ['b','r'];
+legenda = [string(['Plant u = ',num2str(u)]),string(['Model u = ',num2str(u)])];
+strojenieReczne('model7.slx', regP, regPID, sub, kolory, set, legenda);
 
+set = 1;
+u = uInit(2);
+kolory = ['y','c'];
+legenda = [string(['Plant u = ',num2str(u)]),string(['Model u = ',num2str(u)])];
+strojenieReczne('model7.slx', regP, regPID, sub, kolory, set, legenda);
+
+u = uInit(3);
+kolory = ['m','g'];
+legenda = [string(['Plant u = ',num2str(u)]),string(['Model u = ',num2str(u)])];
+strojenieReczne('model7.slx', regP, regPID, sub, kolory, set, legenda);
+%{
 for i = 1:1:length(uInit)
 u = uInit(i);
+sub.F = 1;
+sub.T = 1;
+kolory = ['b','r'];
 
-out = sim('model7.slx'); 
+legenda = [string(['u = ',num2str(u)]),string(['u = ',num2str(u)])];
+strojenieReczne('model7.slx', regP, regPID, sub, kolory, set, legenda);
+set = 1;
 
-% Regulator P
-figure(1);
-subplot(length(uInit),1,i);
-yline(u,'--');
-hold on;
-plot(out.tout,out.yPlantP);
-plot(out.tout,out.yModelP);
-title(['Output, u = ', num2str(u), ', regulator P']);
-ylabel('y [rad]');
-xlabel('time');
-grid on;
-legend('Set value','Plant', 'Model');
-
-% Regulator PID
-figure(2);
-subplot(length(uInit),1,i);
-yline(u,'--');
-hold on;
-plot(out.tout,out.yPlantPID);
-plot(out.tout,out.yModelPID);
-title(['Output, u = ', num2str(u), ', regulator PID']);
-ylabel('y [rad]');
-xlabel('time');
-grid on;
-legend('Set value','Plant', 'Model');
+% out = sim('model7.slx'); 
+% 
+% % Regulator P
+% figure(1);
+% subplot(length(uInit),1,i);
+% yline(u,'--');
+% hold on;
+% plot(out.tout,out.yPlantP);
+% plot(out.tout,out.yModelP);
+% title(['Output, u = ', num2str(u), ', regulator P']);
+% ylabel('y [rad]');
+% xlabel('time');
+% grid on;
+% legend('Set value','Plant', 'Model');
+% 
+% % Regulator PID
+% figure(2);
+% subplot(length(uInit),1,i);
+% yline(u,'--');
+% hold on;
+% plot(out.tout,out.yPlantPID);
+% plot(out.tout,out.yModelPID);
+% title(['Output, u = ', num2str(u), ', regulator PID']);
+% ylabel('y [rad]');
+% xlabel('time');
+% grid on;
+% legend('Set value','Plant', 'Model');
 
 end
-
+%}
 %% Punkt 9 - dokończ go, - dwa optymalne regulatory - parametry
 close all;
 % krP = 0.954623487930613
@@ -191,40 +222,50 @@ close all;
 % td = 1.566874999999999
 
 % Ręczna zmiana nastaw 
-% krP = [0.5, 0.95, 1.2, 2];
 stopTime = 90;
 % wartośc zadana
 u = 1;
-
+%Nastawy reguatorów
 krP = [1.2, 2];
 krPID = [2, 4];
 Ti = [ 7, 7];
 Td = [0.1, 0.9];
-
-freq = 0.1; %Zmiana częstotliwośći
-
+%Zmiana częstotliwośći
+freq = 0.1; 
 % Do bloku chirp
 freqInit = 0.05;
 freqTime = 90;
-freqTarget = 0.2;
+freqTarget = 0.1;
 
 licznik = length(krP);
 for i = 1:1:licznik
-% 
-% modelNazwa = 'model9_sin.slx';    %Wymuszenie sinusem
+ 
 % modelNazwa = 'model9_prost.slx';  %Wymuszenie prostokątnym
-% modelNazwa = 'model9_sinZmienny.slx'; %Wym. sin z zmienną częstotliwością
-modelNazwa = 'model7.slx';        % Wymuszenie step
-regP.kr = krP(i);
+% modelNazwa = 'model9_sin.slx';    %Wymuszenie sinusem
+modelNazwa = 'model9_sinZmienny.slx'; %Wym. sin z zmienną częstotliwością
+% modelNazwa = 'model7.slx';        % Wymuszenie step
 
+regP.kr = krP(i);
 regPID.kr = krPID(i);
 regPID.Ti = Ti(i);
 regPID.Td = Td(i); 
 
-
 sub.F = licznik;
 sub.T = i;
-strojenieReczne(modelNazwa, regP, regPID, sub);
+
+out = strojenieReczne(modelNazwa, regP, regPID, sub);
+
+% % Char bodego
+% dataP = iddata(out.yPlantP,out.setValue,h);
+% dataPID = iddata(out.yPlantPID,out.setValue,h);
+% sysP = tfest(dataP,2)
+% sysPID = tfest(dataPID,4)
+% figure;
+% bode(sysP);
+% title(['kr = ', num2str(regP.kr), ', regulator P']);
+% figure;
+% bode(sysPID);
+% title(['kr=', num2str(regPID.kr), ' Ti=', num2str(regPID.Ti), ' Td=', num2str(regPID.Td) ', regulator PID']);
 
 end
 
@@ -276,8 +317,152 @@ regP.kr = 1.2;  %Prostokątne: Przy większych wzmocnieniach są przeregulowania
 %Sin: Im większe tym lepiej podąża za sinusem.
 % regP.kr = 2;  % Większe przeregulowania, lepeij podąża za sinusem
 
+%% Char bodego
+% wy_1 = out(1);
+% wy_2 = out(2);
+% save('charBodeDane', 'wy_1', 'wy_2')
+% load('charBodeDane.mat');
+
+% Ręczna zmiana nastaw 
+stopTime = 2000;
+% wartośc zadana
+u = 1;
+% Do bloku chirp
+freqInit = 0.05;
+freqTime = 600;
+freqTarget = 0.2;
+
+regP.kr = (1.2 + 2)/2;
+
+out = sim('model9_sinZmienny.slx'); 
+
+figure(1);
+plot(out.tout, out.setValue,'k--');
+hold on;
+plot(out.tout,out.yPlantP, 'b');
+plot(out.tout,out.yModelP, 'r');
+title(['kr = ', num2str(regP.kr), ', regulator P']);
+ylabel('y [rad]');
+xlabel('time');
+grid on;
+legend('Set value','Plant', 'Model');
 
 
+%% 
+
+% windowSize = 100; 
+% b = (1/windowSize)*ones(1,windowSize);
+% a = 1;
+
+[b,a] = butter(4,0.02,'low');
+y = filter(b,a,out.yPlantP);
+
+figure;
+plot(y);
+hold on;
+
+% Znajduje piki
+[pks, locs] = findpeaks(y);
+%wykreśla piki
+
+% scatter(locs, pks,'r');
+% Piki > 0 
+locs = locs(pks>0);
+pks = pks(pks>0);
+% figure;
+scatter(locs, pks,'r');
+
+bodeWar = 20*log10(pks);
+% Bode z danych
+figure;
+plot(locs(1:end), bodeWar(1:end));
+%bODE Z k
+figure;
+bode(K);
+
+% Obie osie mają być w logorytmicznie !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+%%
+dataP = iddata(out.yPlantP,out.setValue,h);
+sysP = tfest(dataP,2)
+figure;
+bode(sysP);
+
+%% Punkt 10 - sisotool
+
+% sisotool(K)
+load('sisoPID.mat');
+%Sisotool - P
+stopTime = 90;
+u = 1;
+regP.kr = 1.1437;
+
+C = pid(C);
+regPID.kr = C.Kp;
+regPID.Ti = C.Ki;
+regPID.Td = C.Kd; 
+
+sub.F = 1;
+sub.T = 1;
+strojenieReczne('model7.slx', regP, regPID, sub);
+
+
+%% 11 - dokończ robercik to !!!!!!!!
+
+stopTime = 90;
+u = 1;
+% Siso tool
+load('sisoNastawy.mat');
+out = sim('model7.slx'); 
+
+% Regulator P - sisotool
+figure(1);
+plot(out.tout, out.setValue,'k--');
+hold on;
+plot(out.tout,out.yPlantP);
+plot(out.tout,out.yModelP);
+
+% Regulator PID - sisotool
+figure(2);
+plot(out.tout, out.setValue,'k--');
+hold on;
+plot(out.tout,out.yPlantPID);
+plot(out.tout,out.yModelPID);
+
+
+
+
+% Regulator P Nastawy analityczne
+load('nastawy.mat');
+out = sim('model7.slx'); 
+plot(out.tout,out.yPlantP);
+plot(out.tout,out.yModelP);
+
+
+% Title i opisy
+title(['kr = ', num2str(regP.kr), ', regulator P']);
+ylabel('y [rad]');
+xlabel('time');
+grid on;
+
+% Regulator PID
+figure(2);
+plot(out.tout, out.setValue,'k--');
+hold on;
+plot(out.tout,out.yPlantPID);
+plot(out.tout,out.yModelPID);
+
+
+
+% Title i opisy
+title(['kr=', num2str(regPID.kr), ' Ti=', num2str(regPID.Ti), ' Td=', num2str(regPID.Td) ', regulator PID']);
+ylabel('y [rad]');
+xlabel('time');
+grid on;
+
+figure(1);
+legend('Set value','Plant', 'Model','Set value', 'SisotoolPlant', 'SisotoolModel');
+figure(2);
+legend('Set value','Plant', 'Model','Set value', 'SisotoolPlant', 'SisotoolModel');
 
 
 
--- strojenieReczne.m
+++ strojenieReczne.m
@@ -1,34 +1,55 @@
-function strojenieReczne(model, regP, regPID, sub)
+function [out] = strojenieReczne(model, regP, regPID, sub, kolor, setVal, legenda)
+% strojenieReczne(model, regP, regPID, sub, kolor, setVal)
+% model     - Nazwa modelu do urochomienia
+% reP       - nastawy regulatora P
+% regPID    - nastawy regulatora PID
+% sub       - sub.F - ile ma być obrazków w oknie, sub.T - który to ma być
+%             obrazek
+% kolor     - kolory do wykresów - tylko podstawowe matlaba w 'r' formacie
+% setVal    - czy ma być wykreślana setValue
+% legenda   - należy dodać legendę w "..." 
+
+if (~exist('kolor','var') || isempty(kolor))
+    kolor = ['b', 'r'];
+end
+
+if (~exist('legend') || isempty(legenda))
+    legenda = ["Plant", "Model"];
+end
 
 out = sim(model); 
-
+set_figure_toscreen(1)
 % Regulator P
 figure(1);
 subplot(sub.F,1,sub.T);
-plot(out.tout, out.setValue,'k--');
+if (~exist('setVal','var') || isempty(setVal))
+    plot(out.tout, out.setValue,'k--','DisplayName','Set value');
+end
 hold on;
-plot(out.tout,out.yPlantP, 'b');
-plot(out.tout,out.yModelP, 'r');
+plot(out.tout,out.yPlantP, kolor(1),'DisplayName',legenda(1));
+plot(out.tout,out.yModelP, kolor(2),'DisplayName',legenda(2));
+% plot(out.tout,out.yPlantP,'r');
+% plot(out.tout,out.yModelP,'b');
 title(['kr = ', num2str(regP.kr), ', regulator P']);
 ylabel('y [rad]');
 xlabel('time');
 grid on;
-legend('Set value','Plant', 'Model');
-
-
+legend show;
 % Regulator PID
 figure(2);
 subplot(sub.F,1,sub.T);   %zmiana j lub k 
-plot(out.tout, out.setValue,'k--');
+if (~exist('setVal','var') || isempty(setVal))
+    plot(out.tout, out.setValue,'k--','DisplayName','Set value');
+end
 hold on;
-plot(out.tout,out.yPlantPID, 'b');
-plot(out.tout,out.yModelPID, 'r');
+plot(out.tout,out.yPlantPID, kolor(1),'DisplayName',legenda(1));
+plot(out.tout,out.yModelPID, kolor(2),'DisplayName',legenda(2));
+% plot(out.tout,out.yPlantPID,'r');
+% plot(out.tout,out.yModelPID,'b');
 title(['kr=', num2str(regPID.kr), ' Ti=', num2str(regPID.Ti), ' Td=', num2str(regPID.Td) ', regulator PID']);
 ylabel('y [rad]');
 xlabel('time');
 grid on;
-legend('Set value','Plant', 'Model');
 
-
-
+legend show;
 end
